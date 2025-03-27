8e092425cec5b3d94578162e429bc6bcac6d5687
bonaberibonaberi1 <onoceaniesales@gmail.com>
2025-03-27 15:36:05 +0000

    Delete lib/core directory

--- lib/core/models/category.dart
+++
@@ -1,8 +0,0 @@
-class Category {
-  final String name;
-  final List<String> images;
-  final int number;
-
-  const Category(
-      {required this.name, required this.images, required this.number});
-}
--- lib/core/models/product_carousel.dart
+++
@@ -1,8 +0,0 @@
-class ProductCarousel {
-  final String image;
-  final String charityLogo;
-  final int numberLikes;
-
-  const ProductCarousel(
-      {required this.image, required this.charityLogo, required this.numberLikes});
-}
