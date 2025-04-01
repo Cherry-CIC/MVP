2c60c292e6759de370ae60d7e7afdf71cb0500c3
bonaberibonaberi1 <onoceaniesales@gmail.com>
2025-04-01 10:01:15 +0100

    Charity week card

--- lib/core/config/app_strings.dart
+++ lib/core/config/app_strings.dart
@@ -20,4 +20,58 @@ class AppStrings {
   static const sendEmail = 'Send email';
   static const forgotPassWordInstruction =
       'Please type your email and we’ll get in touch';
+
+
+  
+  static const dontHaveAccount = "Don't have an account?  ";
+  static const rememberMe = "Remember me  ";
+  static const or = "Or";
+  static const signInApple = "Sign in with Apple";
+  static const signInGoogle = "Sign in with Google";
+  static const loginCherry = "Log in to Cherry"; 
+  static const giveInStyle = "Let's give in style"; 
+  static const goodSeeYou = "Good to see you back!";
+  static const typePassword = "Type your password"; 
+  static const hello = "Hello Alicia!!"; 
+
+
+  static const createAccountText = "Create your account"; 
+  static const easyregistrationText = "The registration is easy."; 
+
+  static const whatEmailText = "What's your email?"; 
+  static const verificationCodeText = "We'll send you a verification code."; 
+
+  static const verifyPhoneText = "Verify your phone number"; 
+  static const verifyPhoneCodeText = "Enter your phone number to receive a verification code."; 
+  static const dataRateText = "Message and data rates may apply."; 
+
+  static const sixDigitCodeText = "Enter the 6-digit code"; 
+  static const verifySixDigitCodeText = "A verification code has been sent to your number."; 
+
+
+  static const experienceText = "Tailor your experience"; 
+  static const preferenceText = "Customize your experience to suit your preferences"; 
+
+
+  static const createPasswordText = "Create a password"; 
+  static const protectPasswordText = "Protect your account by creating a strong password."; 
+
+   
+
+  static const charityOfText = "Charity of"; 
+  static const weekText = "the week"; 
+  static const browseItemsText = "Browse pledged items";
+  static const learMoreText = "and learn more about"; 
+  static const workHereText = "the work the do here!"; 
+
+  static const amazingCharityText = "Amazing Charities,"; 
+  static const amazingWorkText = "Amazing Work         "; 
+  static const seeGoodNewsText = "See some good news here..."; 
+
+
+  static const likedItemsText = "Liked items"; 
+  static const justForYouText = "Just for you";
+
+
+  static const localCharityText = "Local Charities";
 }
