524f25eb2c98f1d3911e18f64c6a4b08ce4269be
bonaberibonaberi1 <onoceaniesales@gmail.com>
2025-04-03 14:11:30 +0100

    local charities #19

---
+++ lib/features/home/widgets/localCharities/local_charities_card.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+ 
+import 'package:cherry_mvp/core/config/config.dart';
+import 'package:cherry_mvp/core/utils/utils.dart'; 
+import 'package:cherry_mvp/core/reusablewidgets/reusablewidgets.dart';
+// import 'package:cherry_mvp/core/models/product_carousel.dart';
+import 'package:cherry_mvp/core/models/model.dart';
+  
+class LocalCharitiesCard extends StatelessWidget {
+  const LocalCharitiesCard({super.key, required this.productCarousel});
+
+  final ProductCarousel productCarousel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.transparent, // Set background color
+      elevation: 4, // Adds a shadow effect
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30), // Set border radius
+      ),
+
+      child: Stack(
+        children: [
+          // Background Image
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network(
+              productCarousel.charityLogo, // Replace with your image path 
+              width: MediaQuery.of(context).size.width/2.35,
+              height: 600, // Adjust height as needed
+
+              fit: BoxFit.cover,
+            ),
+          ),  
+        ],
+      ),
+    );
+  }
+}
