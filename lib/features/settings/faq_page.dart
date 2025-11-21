0f09a33bc5d03b552453ba237253846263861db4
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2025-11-22 00:38:25 +0100

    faq navigation setup

--- lib/features/settings/faq_page.dart
+++ lib/features/settings/faq_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import '../../core/config/app_strings.dart';
 
 class FaqPage extends StatelessWidget {
   const FaqPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text(AppStrings.faqText),
+            floating: true,
+            snap: true,
+          ),
+
+          SliverPadding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).padding.bottom + 16,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
