4774622d1f7a3cbf0c3e4dc8c97b65327712af0a
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2025-11-22 01:14:55 +0100

    faqs model setup

---
+++ lib/core/models/faqs_model.dart
@@ -0,0 +1,19 @@
+class FaqEntry {
+  final String question;
+  final String answer;
+
+  const FaqEntry({
+    required this.question,
+    required this.answer,
+  });
+}
+
+class FaqCategory {
+  final String title;
+  final List<FaqEntry> entries;
+
+  const FaqCategory({
+    required this.title,
+    required this.entries,
+  });
+}
