e60ca2534295160da72528c9d7c38565d54c96d5
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2025-12-04 16:44:36 +0100

    added null safe clause to charity logo

--- lib/features/products/product_page.dart
+++ lib/features/products/product_page.dart
@@ -31,86 +31,90 @@ class ProductPage extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           ProductHeaderCarousel(product),
-          SliverList.list(children: [
-            SellerInformation(
-              user: UserInformation(
-                username: 'John Doe',
-                location: 'New York, USA',
-                reviewsCount: 120,
-                followersCount: 300,
-                followingCount: 150,
-                rating: 3.5,
-                awards: 37,
-                hasBuyerDiscounts: true,
+          SliverList.list(
+            children: [
+              SellerInformation(
+                user: UserInformation(
+                  username: 'John Doe',
+                  location: 'New York, USA',
+                  reviewsCount: 120,
+                  followersCount: 300,
+                  followingCount: 150,
+                  rating: 3.5,
+                  awards: 37,
+                  hasBuyerDiscounts: true,
+                ),
+                charity: product.charity?.imageUrl != null
+                    ? Image.network(product.charity!.imageUrl)
+                    : SizedBox.shrink(),
+                padding: const EdgeInsets.fromLTRB(16, 24, 16, 16),
               ),
-              charity: Image.asset(AppImages.homeStart),
-              padding: const EdgeInsets.fromLTRB(16, 24, 16, 16),
-            ),
-            const Divider(thickness: 8),
-            ProductInformation(
-              product: product,
-              padding: const EdgeInsets.all(16),
-            ),
-            const Divider(thickness: 8),
-            ListTile(
-              title: Text(AppStrings.productPageDescription),
-              titleTextStyle: Theme.of(context).textTheme.titleSmall,
-              subtitle: Text(product.description),
-              subtitleTextStyle: TextStyle(
-                color: Theme.of(context).colorScheme.onSurfaceVariant,
+              const Divider(thickness: 8),
+              ProductInformation(
+                product: product,
+                padding: const EdgeInsets.all(16),
               ),
-            ),
-            const Divider(thickness: 8),
-            ProductHighlightTile(
-              onTap: () {},
-              leadingText: AppStrings.productPageBuyerDiscountActive,
-              trailingText: AppStrings.productPageBuy2Get1HalfPrice,
-              trailingIcon: const Icon(Icons.arrow_forward),
-            ),
-            ProductHighlightTile(
-              onTap: () {},
-              leadingText: AppStrings.productPageOpenToOtherCharities,
-              trailingText: AppStrings.productPageRequestOtherCharity,
-              trailingIcon: Image.asset(
-                AppImages.sale,
-                height: 24,
-                width: 24,
+              const Divider(thickness: 8),
+              ListTile(
+                title: Text(AppStrings.productPageDescription),
+                titleTextStyle: Theme.of(context).textTheme.titleSmall,
+                subtitle: Text(product.description),
+                subtitleTextStyle: TextStyle(
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: SizedBox(
-                      height: 56,
-                      child: OutlinedButton(
-                        onPressed: () {},
-                        child: Text(AppStrings.productPageMakeOffer),
+              const Divider(thickness: 8),
+              ProductHighlightTile(
+                onTap: () {},
+                leadingText: AppStrings.productPageBuyerDiscountActive,
+                trailingText: AppStrings.productPageBuy2Get1HalfPrice,
+                trailingIcon: const Icon(Icons.arrow_forward),
+              ),
+              ProductHighlightTile(
+                onTap: () {},
+                leadingText: AppStrings.productPageOpenToOtherCharities,
+                trailingText: AppStrings.productPageRequestOtherCharity,
+                trailingIcon: Image.asset(
+                  AppImages.sale,
+                  height: 24,
+                  width: 24,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 56,
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          child: Text(AppStrings.productPageMakeOffer),
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: SizedBox(
-                      height: 56,
-                      child: FilledButton(
-                        onPressed: () {
-                          context.read<CheckoutViewModel>().clearBasket();
-                          context.read<CheckoutViewModel>().addItem(product);
-                          context
-                              .read<NavigationProvider>()
-                              .navigateTo(AppRoutes.checkout);
-                        },
-                        child: Text(AppStrings.productPageBuyNow),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: SizedBox(
+                        height: 56,
+                        child: FilledButton(
+                          onPressed: () {
+                            context.read<CheckoutViewModel>().clearBasket();
+                            context.read<CheckoutViewModel>().addItem(product);
+                            context.read<NavigationProvider>().navigateTo(
+                              AppRoutes.checkout,
+                            );
+                          },
+                          child: Text(AppStrings.productPageBuyNow),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: MediaQuery.of(context).padding.bottom),
-          ]),
+              SizedBox(height: MediaQuery.of(context).padding.bottom),
+            ],
+          ),
         ],
       ),
     );
