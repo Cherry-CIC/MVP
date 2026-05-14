59af47353b3f94c7893ddbb2dd2dd30744b9e421
cherry <bradleyvenn@cherry.org.uk>
2026-05-14 16:52:44 +0100

    Set production app identifiers (#420)
    * Set production app identifiers
    
    * Add production Firebase app configs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.cherry_mvp"
+    namespace = "uk.org.cherry.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = localProperties.getProperty("flutter.ndkVersion")
 
@@ -34,8 +34,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cherry_mvp"
+        applicationId = "uk.org.cherry.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 25
