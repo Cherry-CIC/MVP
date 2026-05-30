2e5d811a0ae090aea5ea801914f91fa55c5d9f2e
Asad <asad.flutterdeveloper@gmail.com>
2026-05-30 05:25:43 +0500

    feat: add Flutter localization setup using gen-l10n

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,202 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get authLoginButton => 'Login';
+
+  @override
+  String get authRegisterButton => 'Sign Up';
+
+  @override
+  String get authCreateAccountButton => 'Create Account';
+
+  @override
+  String get authLoginTitle => 'Login';
+
+  @override
+  String get authRegisterTitle => 'Register';
+
+  @override
+  String get authGiveInStyleTagline => 'Give in style';
+
+  @override
+  String get authContinueWithEmail => 'Continue with email';
+
+  @override
+  String get authContinueWithApple => 'Continue with Apple';
+
+  @override
+  String get authContinueWithGoogle => 'Continue with Google';
+
+  @override
+  String get authForgotPassword => 'Forgot Password?';
+
+  @override
+  String get authEmailLabel => 'Email';
+
+  @override
+  String get authEmailHint => 'Enter your email';
+
+  @override
+  String get authPasswordLabel => 'Password';
+
+  @override
+  String get authPasswordHint => 'Enter your password';
+
+  @override
+  String get authUsernameLabel => 'Username';
+
+  @override
+  String get authUsernameHint => 'Enter your username';
+
+  @override
+  String get authFirstNameLabel => 'First Name';
+
+  @override
+  String get authFirstNameHint => 'Enter your first name';
+
+  @override
+  String get authPhoneLabel => 'Phone Number';
+
+  @override
+  String get authPhoneHint => 'Enter your phone number';
+
+  @override
+  String get authConfirmPasswordLabel => 'Confirm Password';
+
+  @override
+  String get authConfirmPasswordHint => 'Confirm your password';
+
+  @override
+  String get authLoginSuccessToast => 'Login Successful';
+
+  @override
+  String get authRegisterSuccessToast => 'Registration Successful';
+
+  @override
+  String get authAlreadyHaveAccountPrompt => 'Already have an account? ';
+
+  @override
+  String get authVerifyEmailBody => 'Check your email inbox for a Verification email';
+
+  @override
+  String get authResendVerificationEmail => 'Resend Verification Email';
+
+  @override
+  String get authForgotPasswordInstruction => 'Please type your email and we\'ll get in touch';
+
+  @override
+  String get authSendEmailButton => 'Send email';
+
+  @override
+  String get authNotYouLink => 'Not you';
+
+  @override
+  String get authUsernameSetupTitle => 'Who is this?';
+
+  @override
+  String get authUsernameSetupSubtitle => 'Enter the name you will like to go by.';
+
+  @override
+  String get authUsernameSetupHint => 'Username';
+
+  @override
+  String get authUsernameTakenError => 'Username is already taken.';
+
+  @override
+  String get authUsernameSaveFailed => 'Could not save username. Please try again.';
+
+  @override
+  String get authConfirmSignOutTitle => 'Confirm Sign out';
+
+  @override
+  String get authConfirmSignOutBody => 'Are you sure you want to sign out?';
+
+  @override
+  String get authSignOutButton => 'Sign out';
+
+  @override
+  String get commonOr => 'or';
+
+  @override
+  String get navHome => 'Home';
+
+  @override
+  String get navInbox => 'Inbox';
+
+  @override
+  String get navGive => 'Give';
+
+  @override
+  String get navSearch => 'Search';
+
+  @override
+  String get navProfile => 'Profile';
+
+  @override
+  String get navDiscoverTitle => 'Discover';
+
+  @override
+  String get navPopularSegment => 'Popular';
+
+  @override
+  String get navSmallerCharitiesSegment => 'Smaller Charities';
+
+  @override
+  String get navLocalToYouSegment => 'Local to you';
+
+  @override
+  String get commonSubmit => 'Submit';
+
+  @override
+  String get commonCancel => 'Cancel';
+
+  @override
+  String get commonContinue => 'Continue';
+
+  @override
+  String get commonRetry => 'Retry';
+
+  @override
+  String get commonOk => 'Ok';
+
+  @override
+  String get commonNext => 'Next';
+
+  @override
+  String get productDescriptionHeading => 'Description';
+
+  @override
+  String get productMakeOfferButton => 'Make Offer';
+
+  @override
+  String get productBuyNowButton => 'Buy Now';
+
+  @override
+  String get productAskSellerButton => 'Ask seller';
+
+  @override
+  String get productNoProductSelected => 'No product selected';
+
+  @override
+  String get checkoutPayButton => 'Pay';
+
+  @override
+  String get checkoutSomethingWentWrongToast => 'oops! Something went wrong';
+
+  @override
+  String get checkoutPaymentSuccessfulToast => 'Payment Successful';
+
+  @override
+  String get checkoutPostcodeRequiredToast => 'Postcode required';
+
+  @override
+  String get checkoutChangePickupPoint => 'Change pickup point';
+}
